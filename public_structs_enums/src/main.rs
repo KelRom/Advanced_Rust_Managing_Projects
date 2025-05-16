9d244587f36c70eb3620f040041f32f55c367814
Kelvin Roman Domenech <kerom97@gmail.com>
2025-05-15 23:41:05 -0500

    learned to put structs and enums into a module and using the pub keyword on them and the associated fields that required that type of visibility

---
+++ public_structs_enums/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+
+    // fields are private by default and will have to mark it as pub along with any functions we are going to use a pub if needed
+    // enums when made public all of its variants are public
+    let rect: shape::Rectangle = shape::Rectangle::new(1.2, 3.4);
+    println!("rect area is {}", rect.get_area());
+    println!("rect width is {}", rect.width);
+
+    let cirle: shape::Shape = shape::Shape::Circle;
+}
+
+mod shape {
+
+    pub struct Rectangle {
+        pub width: f64,
+        height: f64
+    }
+
+    pub enum Shape {
+        Rectangle, 
+        Circle,
+        Triangle
+    }
+    impl Rectangle {
+        pub fn new(width: f64, height: f64) -> Rectangle {
+            Rectangle{
+            width,
+            height
+        }
+    }
+
+        pub fn get_area(&self) -> f64 {
+            self.width * self.height
+        }
+    }
+
+}
